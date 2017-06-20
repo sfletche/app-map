22af3bc01ba178be7ef8b5c96b450a2ee302cde9
sfletche <sfletche@gmail.com>
2017-06-20 16:34:44 -0700

    adding icons

---
+++ src/applications/funded-apps.js
@@ -0,0 +1,16 @@
+export const fundedApps = [
+  {
+    "lat_lon": [37.56, -122.33],
+    "message": "San Mateo"
+  },
+  {
+    "lat_lon": [35.37, -119.02],
+    "message": "Bakersfield"
+  },
+  {
+    "lat_lon": [36.75, -119.77],
+    "message": "Fresno"
+  }
+];
+
+
--- src/components/california.js
+++ src/components/california.js
@@ -8,11 +8,11 @@ import {
   LayerGroup,
 } from 'react-leaflet';
 import { activeApps } from '../applications/active-apps';
+import { fundedApps } from '../applications/funded-apps';
 import { basemaps } from '../constants/basemaps';
+import { greenIcon, blueIcon } from '../constants/icons';
 
 const { BaseLayer, Overlay } = LayersControl;
-const mapCenter = [37.5, -120];
-const zoomLevel = 6;
 
 function getBasemaps(basemaps) {
   return basemaps.map(basemap => (
@@ -22,28 +22,35 @@ function getBasemaps(basemaps) {
   ));
 }
 
-function getMarkers(apps) {
+function getMarkers(apps, icon) {
   return apps.map(app => (
-    <Marker key={app.lat_lon} position={app.lat_lon}>
+    <Marker key={app.lat_lon} position={app.lat_lon} icon={icon}>
       <Popup><span>{app.message}</span></Popup>
     </Marker>
   ));
 }
 
-export default () => (
-  <div>
-    <h1>California Applications</h1>
-    <Map center={mapCenter} zoom={zoomLevel}>
-      <LayersControl position="topright">
-        {getBasemaps(basemaps)}
+function California() {
+  const mapCenter = [37.5, -120];
+  const zoomLevel = 6;
+  return (
+    <div>
+      <h1>California Applications</h1>
+      <Map center={mapCenter} zoom={zoomLevel}>
+        <LayersControl position="topright">
+          {getBasemaps(basemaps)}
 
-        <Overlay checked name="Prospects">
-          <LayerGroup>
-            {getMarkers(activeApps)}
-          </LayerGroup>
-        </Overlay>
+          <Overlay checked name="Active Applications">
+            <LayerGroup>{getMarkers(activeApps, blueIcon)}</LayerGroup>
+          </Overlay>
+          <Overlay name="Funded Applications">
+            <LayerGroup>{getMarkers(fundedApps, greenIcon)}</LayerGroup>
+          </Overlay>
 
-      </LayersControl>
-    </Map>
-  </div>
-);
+        </LayersControl>
+      </Map>
+    </div>
+  );
+}
+
+export default California;
