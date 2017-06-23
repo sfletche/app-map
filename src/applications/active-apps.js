4a42530bdca7ec6cf182258bd328e2d2648a4e9e
sfletche <sfletche@gmail.com>
2017-06-22 22:50:24 -0700

    updating active and funded apps

--- src/applications/active-apps.js
+++ src/applications/active-apps.js
@@ -1,16 +1,56 @@
 export const activeApps = [
   {
-    "lat_lon": [37.77, -122.42],
-    "message": "San Francisco"
+    "lat_lon": [35.142467, -119.456508],
+    "message": "Taft"
   },
   {
-    "lat_lon": [38.81, -122.27],
-    "message": "Oakland"
+    "lat_lon": [35.132189, -118.448975],
+    "message": "Tehachapi"
   },
   {
-    "lat_lon": [38.58, -121.49],
-    "message": "Sacramento"
-  }
+    "lat_lon": [40.027103, -122.123322],
+    "message": "Tehama"
+  },
+  {
+    "lat_lon": [33.493639, -117.148364],
+    "message": "Temecula"
+  },
+  {
+    "lat_lon": [34.107231, -118.057847],
+    "message": "Thousand Oaks"
+  },
+  {
+    "lat_lon": [33.83585,  -118.340628],
+    "message": "Torrance"
+  },
+  {
+    "lat_lon": [37.73965,  -121.425222],
+    "message": "Tracy"
+  },
+  {
+    "lat_lon": [41.059292, -124.143125],
+    "message": "Trinidad"
+  },
+  {
+    "lat_lon": [36.207728, -119.347339],
+    "message": "Tulare"
+  },
+  {
+    "lat_lon": [41.955989, -121.477492],
+    "message": "Tulelake"
+  },
+  {
+    "lat_lon": [37.494658, -120.846594],
+    "message": "Turlock"
+  },
+  {
+    "lat_lon": [33.74585,  -117.826167],
+    "message": "Tustin"
+  },
+  {
+    "lat_lon": [34.135558, -116.054169],
+    "message": "Twentynine Palms"
+  },
 ];
 
 
--- src/applications/funded-apps.js
+++ src/applications/funded-apps.js
@@ -1,16 +1,264 @@
 export const fundedApps = [
   {
-    "lat_lon": [37.56, -122.33],
+    "lat_lon": [38.581572, -121.494400],
+    "message": "Sacramento"
+  },
+  {
+    "lat_lon": [36.677736, -121.655500],
+    "message": "Salinas"
+  },
+  {
+    "lat_lon": [34.108344, -117.289764],
+    "message": "San Bernardino"
+  },
+  {
+    "lat_lon": [37.630489, -122.411083],
+    "message": "San Bruno"
+  },
+  {
+    "lat_lon": [37.507158, -122.260522],
+    "message": "San Carlos"
+  },
+  {
+    "lat_lon": [33.426972, -117.611992],
+    "message": "San Clemente"
+  },
+  {
+    "lat_lon": [32.715328, -117.157256],
+    "message": "San Diego"
+  },
+  {
+    "lat_lon": [34.106675, -117.806725],
+    "message": "San Dimas"
+  },
+  {
+    "lat_lon": [34.281947, -118.438972],
+    "message": "San Fernando"
+  },
+  {
+    "lat_lon": [37.774931, -122.419417],
+    "message": "San Francisco"
+  },
+  {
+    "lat_lon": [34.096111, -118.105833],
+    "message": "San Gabriel"
+  },
+  {
+    "lat_lon": [33.783908, -116.958636],
+    "message": "San Jacinto"
+  },
+  {
+    "lat_lon": [36.606617, -120.189044],
+    "message": "San Joaquin"
+  },
+  {
+    "lat_lon": [37.339386, -121.894956],
+    "message": "San Jose"
+  },
+  {
+    "lat_lon": [36.845511, -121.537997],
+    "message": "San Juan Bautista"
+  },
+  {
+    "lat_lon": [33.501692, -117.662550],
+    "message": "San Juan Capistrano"
+  },
+  {
+    "lat_lon": [37.724931, -122.156078],
+    "message": "San Leandro"
+  },
+  {
+    "lat_lon": [35.282753, -120.659617],
+    "message": "San Luis Obispo"
+  },
+  {
+    "lat_lon": [33.143372, -117.166144],
+    "message": "San Marcos"
+  },
+  {
+    "lat_lon": [34.121397, -118.106458],
+    "message": "San Marino"
+  },
+  {
+    "lat_lon": [37.562992, -122.325525],
     "message": "San Mateo"
   },
   {
-    "lat_lon": [35.37, -119.02],
-    "message": "Bakersfield"
+    "lat_lon": [37.962147, -122.345525],
+    "message": "San Pablo"
   },
   {
-    "lat_lon": [36.75, -119.77],
-    "message": "Fresno"
-  }
+    "lat_lon": [37.973536, -122.531086],
+    "message": "San Rafael"
+  },
+  {
+    "lat_lon": [37.779928, -121.978014],
+    "message": "San Ramon"
+  },
+  {
+    "lat_lon": [36.617183, -121.848286],
+    "message": "Sand City"
+  },
+  {
+    "lat_lon": [36.708006, -119.555964],
+    "message": "Sanger"
+  },
+  {
+    "lat_lon": [33.745572, -117.867833],
+    "message": "Santa Ana"
+  },
+  {
+    "lat_lon": [34.420831, -119.698189],
+    "message": "Santa Barbara"
+  },
+  {
+    "lat_lon": [37.354108, -121.955236],
+    "message": "Santa Clara"
+  },
+  {
+    "lat_lon": [34.391664, -118.542586],
+    "message": "Santa Clarita"
+  },
+  {
+    "lat_lon": [36.974117, -122.030797],
+    "message": "Santa Cruz"
+  },
+  {
+    "lat_lon": [33.947236, -118.085344],
+    "message": "Santa Fe Springs"
+  },
+  {
+    "lat_lon": [34.953033, -120.435719],
+    "message": "Santa Maria"
+  },
+  {
+    "lat_lon": [34.019453, -118.491192],
+    "message": "Santa Monica"
+  },
+  {
+    "lat_lon": [34.354167, -119.059269],
+    "message": "Santa Paula"
+  },
+  {
+    "lat_lon": [38.440467, -122.714431],
+    "message": "Santa Rosa"
+  },
+  {
+    "lat_lon": [32.838383, -116.973917],
+    "message": "Santee"
+  },
+  {
+    "lat_lon": [37.263833, -122.023014],
+    "message": "Saratoga"
+  },
+  {
+    "lat_lon": [37.859094, -122.485250],
+    "message": "Sausalito"
+  },
+  {
+    "lat_lon": [37.051061, -122.014683],
+    "message": "Scotts Valley"
+  },
+  {
+    "lat_lon": [33.741406, -118.104786],
+    "message": "Seal Beach"
+  },
+  {
+    "lat_lon": [33.819361, -118.366647],
+    "message": "Seaside"
+  },
+  {
+    "lat_lon": [38.402136, -122.823881],
+    "message": "Sebastopol"
+  },
+  {
+    "lat_lon": [36.570783, -119.612075],
+    "message": "Selma"
+  },
+  {
+    "lat_lon": [35.500514, -119.271775],
+    "message": "Shafter"
+  },
+  {
+    "lat_lon": [40.680428, -122.370842],
+    "message": "Shasta Lake"
+  },
+  {
+    "lat_lon": [34.161672, -118.052847],
+    "message": "Sierra Madre"
+  },
+  {
+    "lat_lon": [33.804461, -118.167847],
+    "message": "Signal Hill"
+  },
+  {
+    "lat_lon": [34.269447, -118.781483],
+    "message": "Simi Valley"
+  },
+  {
+    "lat_lon": [32.991156, -117.271147],
+    "message": "Soledad"
+  },
+  {
+    "lat_lon": [34.595819, -120.137647],
+    "message": "Solvang"
+  },
+  {
+    "lat_lon": [38.291858, -122.458036],
+    "message": "Sonoma"
+  },
+  {
+    "lat_lon": [37.984092, -120.382139],
+    "message": "Sonora"
+  },
+  {
+    "lat_lon": [34.051956, -118.046733],
+    "message": "South El Monte"
+  },
+  {
+    "lat_lon": [33.954736, -118.212017],
+    "message": "South Gate"
+  },
+  {
+    "lat_lon": [38.939925, -119.977186],
+    "message": "South Lake Tahoe"
+  },
+  {
+    "lat_lon": [34.116119, -118.150350],
+    "message": "South Pasadena"
+  },
+  {
+    "lat_lon": [37.654656, -122.407750],
+    "message": "South San Francisco"
+  },
+  {
+    "lat_lon": [38.505242, -122.470386],
+    "message": "St. Helena"
+  },
+  {
+    "lat_lon": [33.802517, -117.993117],
+    "message": "Stanton"
+  },
+  {
+    "lat_lon": [37.957703, -121.290781],
+    "message": "Stockton"
+  },
+  {
+    "lat_lon": [38.238247, -122.040244],
+    "message": "Suisun City"
+  },
+  {
+    "lat_lon": [37.368831, -122.036350],
+    "message": "Sunnyvale"
+  },
+  {
+    "lat_lon": [40.416283, -120.653006],
+    "message": "Susanville"
+  },
+  {
+    "lat_lon": [38.392967, -120.802436],
+    "message": "Sutter Creek"
+  },
 ];
 
 
